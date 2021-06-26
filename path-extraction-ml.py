8d792ad0733e808c5eb43e45dbb1591a6eb2990c
neenerrh <59638308+neenerrh@users.noreply.github.com>
2021-06-26 13:57:30 +0800

    Update path-extraction-ml.py

--- path-extraction-ml.py
+++ path-extraction-ml.py
@@ -20,12 +20,12 @@ def load_data(file):
     for line in file:
         lines = line.split('\t')
         user = lines[0]
-        movie = lines[1].replace('\n','')
-        data.append((user, movie))
+        video= lines[1].replace('\n','')
+        data.append((user, video))
     return data
 
 
-def add_user_movie_interaction_into_graph(positive_rating):
+def add_user_video_interaction_into_graph(positive_rating):
     '''
     add user-movie interaction data into the graph
 
@@ -39,12 +39,16 @@ def add_user_movie_interaction_into_graph(positive_rating):
 
     for pair in positive_rating:
         user = pair[0]
-        movie = pair[1]
-        user_node = user
-        movie_node =  movie
+
+        video = pair[1]
+        user_node = 'u'+user
+        video_node =  'v'+ video
+        
         Graph.add_node(user_node)
-        Graph.add_node(movie_node)
-        Graph.add_edge(user_node, movie_node)
+        Graph.add_node(video_node)
+        Graph.add_edge(user_node, video_node)
+
+    
 
     return Graph
 
@@ -62,7 +66,7 @@ def add_course_concept(fr_courseconcept_file,Graph):
         concept = lines[1]
 
         course_node = course
-        concept_node = 'K_' + concept
+        concept_node = 'k' + concept
         
         if not Graph.has_node(course_node):
             Graph.add_node(course_node)
@@ -83,32 +87,39 @@ def add_course_concept(fr_courseconcept_file,Graph):
 
     return Graph     
     
-def add_concept_concept(fr_conceptconcept_file,Graph):
+def add_concept_concept(fr_conceptconcept_file, Graph ,fw_edges):
       
 
     for line in fr_conceptconcept_file:
         lines = line.split('\t')
-        concept1 = lines[0]
-        #print(concept1)
+        concept = lines[0]
+        #print(concept)
         concept2 = lines[1]
         #print(concept2)
         
         
-        concept1_node = 'K_' + concept1
-        concept2_node = 'K_' + concept2
-        
-        if not Graph.has_node(concept1_node):
-            Graph.add_node(concept1_node)
-            
+        concept1_node = 'k' + concept
+        concept2_node = 'k' + concept2
+    
+        #print(concept_node)
+        #print(Graph.has_node(concept_node))
+        #if not Graph.has_node(concept1_node):
+            #Graph.add_node(concept1_node)
+        #print(Graph.has_node(concept2_node))    
         if not Graph.has_node(concept2_node):
             Graph.add_node(concept2_node)
-            
+        #print(concept2_node)    
+        #Graph.add_edge(concept1_node,concept2_node)
+        #print(concept1_node+','+concept2_node)
         Graph.add_edge(concept1_node, concept2_node)
-        #Graph.add_edge(video_node, concept1_node)     
+        #Graph.add_edge(concept2_node, concept1_node)     
         
-       
+        #print(concept1_node)
+        #print(Graph.has_edge(concept1_node,concept2_node))
+        #break
+    nx.write_edgelist(Graph, fw_edges)
 
-    return Graph 
+    return Graph,concept1_node,concept2_node 
     
 def add_vid_concept(fr_vidconcept_file,Graph):
       
@@ -119,10 +130,10 @@ def add_vid_concept(fr_vidconcept_file,Graph):
         concept = lines[1]
         
         
-        video_node = video
-        concept_node = 'K_' + concept
+        video_node = 'v'+ video
+        concept_node = 'k' + concept
         
-        
+        concept_node= concept_node.replace('\n', '')
         
         if not Graph.has_node(video_node):
             Graph.add_node(video_node)
@@ -131,10 +142,10 @@ def add_vid_concept(fr_vidconcept_file,Graph):
             Graph.add_node(concept_node)
             
         Graph.add_edge(video_node, concept_node)
-        Graph.add_edge(concept_node, video_node)     
-        
-       
+        Graph.add_edge(concept_node, video_node) 
 
+     
+    
     return Graph    
         
 def add_course_video(fr_coursevideo_file,Graph):
@@ -146,8 +157,8 @@ def add_course_video(fr_coursevideo_file,Graph):
         video = lines[1]
         
         
-        course_node = course
-        video_node = video
+        course_node = 'c'+ course
+        video_node = 'v'+ video
         
         if not Graph.has_node(course_node):
             Graph.add_node(course_node)
@@ -171,8 +182,8 @@ def add_user_course(fr_usercourse_file,Graph):
         course = lines[1]
         
         
-        user_node = user
-        course_node = course
+        user_node = 'u'+ user
+        course_node = 'c' + course
         
         if not Graph.has_node(user_node):
             Graph.add_node(user_node)
@@ -254,7 +265,7 @@ def print_graph_statistic(Graph):
     print('The number of edges is  ' + str(len(Graph.edges()))+ ' \n')
 
 
-def mine_paths_between_nodes(Graph, user_node, movie_node, maxLen, sample_size, fw_file):
+def mine_paths_between_nodes(Graph, user_node, video_node, maxLen, sample_size, fw_file):
     '''
     mine qualified paths between user and movie nodes, and get sampled paths between nodes
 
@@ -264,22 +275,34 @@ def mine_paths_between_nodes(Graph, user_node, movie_node, maxLen, sample_size, 
         @maxLen: path length
         @fw_file: the output file for the mined paths
     ''' 
-
+    #print(user_node)
+    #print(video_node)
     connected_path = [] 
-    for path in nx.all_simple_paths(Graph, source=user_node, target=movie_node, cutoff=maxLen):
+    for path in nx.all_simple_paths(Graph, source=user_node, target=video_node, cutoff=maxLen):
+        
+        
         if len(path) == maxLen + 1: 
             connected_path.append(path)
 
     path_size = len(connected_path)
-
+    #print(path_size)
     #as there is a huge number of paths connected user-movie nodes, we get randomly sampled paths
     #random sample can better balance the data distribution and model complexity
     if path_size > sample_size:
         random.shuffle(connected_path)
         connected_path = connected_path[:sample_size]
-    
+    if not connected_path:
+        print("No path between nodes")
     for path in connected_path:
+        print(path)
+        #path1 =[]
+        #for element in path:
+            #path1.append(element.strip())
+        
+        #path=path.rstrip("\n")
+        #print(path1)
         line = ",".join(path) + '\n'
+        #print(line)
         fw_file.write(line)
    
     #print('The number of paths between '+ user_node + ' and ' + movie_node + ' is: ' +  str(len(connected_path)) +'\n')
@@ -297,24 +320,25 @@ def dump_paths(Graph, rating_pair, maxLen, sample_size, fw_file):
     '''
     for pair in rating_pair:
         user_id = pair[0]
-        movie_id = pair[1]
-        user_node =  user_id
-        movie_node =  movie_id
-
-        if Graph.has_node(user_node) and Graph.has_node(movie_node):
-            mine_paths_between_nodes(Graph, user_node, movie_node, maxLen, sample_size, fw_file)
-
+        video_id = pair[1]
+        user_node =  'u'+user_id
+        video_node = 'v'+ video_id
+        #print(user_node+','+video_node)
+        if Graph.has_node(user_node) and Graph.has_node(video_node):
+            print(user_node+','+video_node)
+            mine_paths_between_nodes(Graph, user_node, video_node, maxLen, sample_size, fw_file)
+            
 
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description=''' Build Knowledge Graph and Mine the Connected Paths''')
     parser.add_argument('--training', type=str, dest='training_file', default='data/mooc/training.txt')
     parser.add_argument('--negtive', type=str, dest='negative_file', default='data/mooc/negative.txt')
-    parser.add_argument('--usercourse', type=str, dest='usercourse_file', default='data/mooc/user_course.csv')
-    parser.add_argument('--coursevideo', type=str, dest='coursevideo_file', default='data/mooc/course_video.txt')  
-    parser.add_argument('--vidconcept', type=str, dest='vidconcept_file', default='data/mooc/video_concept.txt')
+    #parser.add_argument('--usercourse', type=str, dest='usercourse_file', default='data/mooc/u_c.csv')
+    #parser.add_argument('--coursevideo', type=str, dest='coursevideo_file', default='data/mooc/course_video.txt')  
+    parser.add_argument('--vidconcept', type=str, dest='vidconcept_file', default='data/mooc/vid_con.txt')
     parser.add_argument('--conceptconcept', type=str, dest='conceptconcept_file', default='data/mooc/concept_concept.txt')
-    parser.add_argument('--courseconcept', type=str, dest='courseconcept_file', default='data/mooc/course_concept.txt')
+    #parser.add_argument('--courseconcept', type=str, dest='courseconcept_file', default='data/mooc/course_concept.txt')
 
     #parser.add_argument('--auxiliary', type=str, dest='auxiliary_file', default='data/mooc/auxiliary-mapping.txt')
     
@@ -322,36 +346,40 @@ if __name__ == '__main__':
                         help='paths between user-item interaction pairs')
     parser.add_argument('--negativepath', type=str, dest='negative_path', default='data/mooc/negative-path.txt', \
                         help='paths between negative sampled user-item pair')
-    parser.add_argument('--pathlength', type=int, dest='path_length', default=5, help='length of paths with choices [3,5,7]')
+    parser.add_argument('--pathlength', type=int, dest='path_length', default=8, help='length of paths with choices [3,5,7]')
     parser.add_argument('--samplesize', type=int, dest='sample_size', default=5, \
                         help='the sampled size of paths bwteen nodes with choices [5, 10, 20, ...]')
+    parser.add_argument('--edges', type=str, dest='edges', default='data/mooc/edges.txt', \
+                        help='edgesin graph')
 
     parsed_args = parser.parse_args()
     
     training_file = parsed_args.training_file
     negative_file = parsed_args.negative_file
-    usercourse_file =  parsed_args.usercourse_file
-    coursevideo_file =  parsed_args.coursevideo_file
+    #usercourse_file =  parsed_args.usercourse_file
+    #coursevideo_file =  parsed_args.coursevideo_file
     vidconcept_file = parsed_args.vidconcept_file
     conceptconcept_file= parsed_args.conceptconcept_file
-    courseconcept_file = parsed_args.courseconcept_file
+    #courseconcept_file = parsed_args.courseconcept_file
     
     #auxiliary_file = parsed_args.auxiliary_file
     positive_path = parsed_args.positive_path
     negative_path = parsed_args.negative_path
     path_length = parsed_args.path_length
     sample_size = parsed_args.sample_size
+    edges = parsed_args.edges
     
     fr_training = open(training_file,'r')
     fr_negative = open(negative_file, 'r')
-    fr_usercourse_file =   open(usercourse_file, 'r', encoding="utf-8") 
-    fr_coursevideo_file =   open(coursevideo_file, 'r', encoding="utf-8")
+    #fr_usercourse_file =   open(usercourse_file, 'r', encoding="utf-8") 
+    #fr_coursevideo_file =   open(coursevideo_file, 'r', encoding="utf-8")
     fr_vidconcept_file =   open(vidconcept_file, 'r', encoding='utf-8') 
     fr_conceptconcept_file =   open(conceptconcept_file, 'r', encoding='utf-8') 
-    fr_courseconcept_file =   open(courseconcept_file, 'r', encoding='utf-8') 
+    #fr_courseconcept_file =   open(courseconcept_file, 'r', encoding='utf-8') 
     #fr_auxiliary = open(auxiliary_file,'r')
     fw_positive_path = open(positive_path, 'w')
     fw_negative_path = open(negative_path, 'w')
+    fw_edges = open(edges, 'wb')
     
     positive_rating = load_data(fr_training)
     negative_rating = load_data(fr_negative)
@@ -359,20 +387,54 @@ if __name__ == '__main__':
     print('The number of user-movie interaction data is:  ' + str(len(positive_rating))+ ' \n')
     print('The number of negative sampled data is:  ' + str(len(negative_rating))+ ' \n') 
 
-    Graph = add_user_movie_interaction_into_graph(positive_rating)
-    Graph=add_user_course(fr_usercourse_file,Graph)
-    Graph=add_course_video(fr_coursevideo_file,Graph)
-    Graph=add_vid_concept(fr_vidconcept_file,Graph)
-    Graph=add_concept_concept(fr_conceptconcept_file,Graph)
-    Graph=add_vid_concept(fr_vidconcept_file,Graph)
-    
-    Graph=add_course_concept(fr_courseconcept_file,Graph)
-    #Graph = add_auxiliary_into_graph(fr_auxiliary, Graph)
+    Graph = add_user_video_interaction_into_graph(positive_rating)
     print_graph_statistic(Graph)
+ 
+    #Graph=add_user_course(fr_usercourse_file,Graph)
+
+    #Graph=add_course_video(fr_coursevideo_file,Graph)
+   
+    Graph = add_vid_concept(fr_vidconcept_file,Graph)
+    print_graph_statistic(Graph)
+    Graph,node1,node2=add_concept_concept(fr_conceptconcept_file,Graph,fw_edges)
+    
+    
+    
+    
+    #Graph=add_course_concept(fr_courseconcept_file,Graph)
+    #Graph = add_auxiliary_into_graph(fr_auxiliary, Graph)
+   
+    print_graph_statistic(Graph)
+
+
+   
+    
+
+    #nx.write_edgelist(Graph, fw_edges)
+    #print(Graph.has_edge('v9059','k14290'))
+    #print(Graph.has_edge('k14290', 'k44977'))
+    
+    #print(Graph.has_edge('k14290','k44977'))
+    #print(Graph.has_edge('k23398','k44977'))
+    #print(Graph.has_edge('k58621','k44977'))
+    #print(Graph.has_edge('k102799','k84993'))
+
+
+    #print(Graph.has_edge('v29391', 'k96589'))
+    
+    #print(Graph.edges( ))
+    
+    
+    
+    
+
+
+    #nx.draw_networkx(Graph)
+    #plt.show()
 
     dump_paths(Graph, positive_rating, path_length, sample_size, fw_positive_path)
     dump_paths(Graph, negative_rating, path_length, sample_size, fw_negative_path)
-    print("Done")
+    
 
     fr_training.close()
     fr_negative.close()
