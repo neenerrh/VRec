c3c96b103be279afbd54d236f644fb60d5471087
neenerrh <59638308+neenerrh@users.noreply.github.com>
2021-07-10 17:35:46 +0800

    Update recurrent-neural-network.py

--- recurrent-neural-network.py
+++ recurrent-neural-network.py
@@ -1,237 +1,89 @@
-#This is part is aims to feed connected paths into the recurrent neural network to train and test the proposed methods.
-
 import numpy as np
-import argparse
 import torch
 import torch.autograd as autograd
 from torch import nn, optim
 from torch.autograd import Variable
 import torch.nn.functional as F
-from LSTMTagger import LSTMTagger
-from LSTMTrain import LSTMTrain
-from LSTMEvaluation import LSTMEvaluation
-from datetime import datetime
 
-
-def load_paths(fr_file, isPositive):
+class LSTMTrain(object):
 	'''
-	load postive or negative paths, map all nodes in paths into ids
-
-	Inputs:
-		@fr_file: positive or negative paths
-		@isPositive: identify fr_file is positive or negative
+	recurrent neural network training process
 	'''
-
-	global node_count, all_variables, paths_between_pairs, positive_label, all_user, all_movie
-	
-	for line in fr_file:
-		line = line.replace('\n', '')
-		lines = line.split(',')
-		user = lines[0]
-		movie = lines[-1]
-
-		if user not in all_user:
-			all_user.append(user)
-		if movie not in all_movie:
-			all_movie.append(movie)
-
-		key = (user, movie)
-		value = []
-		path = []
-
-		if isPositive:
-			if key not in positive_label:
-				positive_label.append(key)
-
-		for node in lines:
-			if node not in all_variables:
-				all_variables.update({node:node_count})
-				node_count = node_count + 1
-			path.append(node)
-
-		if key not in paths_between_pairs:
-			value.append(path)
-			paths_between_pairs.update({key:value})
-		else:
-			paths_between_pairs[key].append(path)
+	def __init__(self, model, iteration, learning_rate, paths_between_pairs, positive_label, \
+		all_variables, all_user, all_movie):
+		super(LSTMTrain, self).__init__()
+		self.model = model
+		self.iteration = iteration
+		self.learning_rate = learning_rate
+		self.paths_between_pairs = paths_between_pairs
+		self.positive_label = positive_label
+		self.all_variables = all_variables
+		self.all_user = all_user
+		self.all_movie = all_movie
 
 
-def load_pre_embedding(fr_pre_file, isUser):
-	'''
-	load pre-train-user or movie embeddings
+	def dump_post_embedding(self):
+		'''
+		dump the post-train user or item embedding
+		'''
+		embedding_dict = {}
+		node_list = self.all_user + self.all_movie
 
-	Inputs:
-		@fr_pre_file: pre-train-user or -movie embeddings
-		@isUser: identify user or movie
-	'''
-	global pre_embedding, all_variables
+		for node in node_list:
+			node_id = torch.LongTensor([int(self.all_variables[node])])
+			node_id = Variable(node_id)
+			if torch.cuda.is_available():
+				node_id = node_id.cuda()
+			node_embedding = self.model.embedding(node_id).squeeze().cpu().data.numpy()
+			if node not in embedding_dict:
+				embedding_dict.update({node:node_embedding})
 
-	for line in fr_pre_file:
-		lines = line.split('|')
-		node = lines[0]
-		if isUser:
-			node = 'u' + node
-		else:
-			node = 'v' + node
+		return embedding_dict
+			
+		
+	def train(self):
+		criterion = nn.BCELoss()
+		#You may also try different types of optimization methods (e.g, SGD, RMSprop, Adam, Adadelta, etc.)
+		optimizer = optim.SGD(self.model.parameters(), lr=self.learning_rate)
+		#optimizer = optim.RMSprop(self.model.parameters(), lr=self.learning_rate)
 
-		if node in all_variables:
-			node_id = all_variables[node]
-			embedding = [float(x) for x in lines[1].split()]
-			embedding = np.array(embedding)
-			pre_embedding[node_id] = embedding
+		for epoch in range (self.iteration):
+			running_loss = 0.0
+			data_size = len(self.paths_between_pairs)
+			label = Variable(torch.Tensor())    
+			
+			for pair in self.paths_between_pairs:
+				
+				paths_between_one_pair = self.paths_between_pairs[pair]
+				paths_between_one_pair_size = len(paths_between_one_pair)
+				paths_between_one_pair_id = []
 
+				for path in paths_between_one_pair:
+					path_id = [self.all_variables[x] for x in path]
+					paths_between_one_pair_id.append(path_id) 
+			
+				paths_between_one_pair_id = np.array(paths_between_one_pair_id)
+				paths_between_one_pair_id = Variable(torch.LongTensor(paths_between_one_pair_id))
+					
+				
+				if torch.cuda.is_available():
+					paths_between_one_pair_id = paths_between_one_pair_id.cuda()
+				
+				out = self.model(paths_between_one_pair_id)
+				out = out.squeeze()
+			
+				if pair in self.positive_label:
+					label = Variable(torch.Tensor([1]))
+				else:
+					label = Variable(torch.Tensor([0])) 
+			
+				loss = criterion(out.cpu(), label)
+				running_loss += loss.item() * label.item()
+				
+				optimizer.zero_grad()
+				loss.backward()
+				optimizer.step()
+			
+			print('epoch['+str(epoch) + ']: loss is '+str(running_loss))
 
-def load_data(fr_file):
-	'''
-	load training or test data
-
-	Input: 
-			@fr_rating: the user-item rating data
-
-	Output:
-			@rating_data: user-specific rating data with timestamp
-	'''
-
-	data_dict = {}
-
-	for line in fr_file:
-			lines = line.replace('\n', '').split('\t')
-			user = 'u' + lines[0]
-			item = 'v' + lines[1]
-
-			if user not in data_dict:
-				data_dict.update({user:[item]})
-			elif item not in data_dict[user]:
-				data_dict[user].append(item)
-
-	return data_dict
-
-
-def write_results(fw_results, precision_1, precision_5, precision_10, mrr_10):
-	'''
-	write results into text file
-	'''
-	line = 'precision@1: ' + str(precision_1) + '\n' + 'precision@5: ' + str(precision_5) + '\n' \
-		+ 'precision@10: ' + str(precision_10) + '\n' + 'mrr: ' + str(mrr_10) + '\n'
-	fw_results.write(line)
-
-
-
-if __name__ == '__main__':
-	
-	parser = argparse.ArgumentParser(description=''' Recurrent Neural Network ''')
-
-	'''
-	Parameter Settings: 
-	for MovieLens in terms of [input_dim, hidden_dim, out_dim, iteration, learning_rate, optimizer] is [10, 16, 1, 5, 0.2/0.1, SGD]
-	for Yelp in terms of [input_dim, hidden_dim, out_dim, iteration, learning_rate, optimizer] is [20, 32, 1, 5, 0.01, SGD]
-	You can change optimizer in the LSTMTrain class
-	'''
-
-	parser.add_argument('--inputdim', type=int, dest='input_dim', default=10)
-	parser.add_argument('--hiddendim', type=int, dest='hidden_dim', default=16)
-	parser.add_argument('--outdim', type=int, dest='out_dim', default=1)
-	parser.add_argument('--iteration', type=int, dest='iteration', default=5)
-	parser.add_argument('--learingrate', type=float, dest='learning_rate', default=0.2)
-	
-	parser.add_argument('--positivepath', type=str, dest='positive_path', default='data/mooc/positive-path1.txt')
-	parser.add_argument('--negativepath', type=str, dest='negative_path', default='data/mooc/negative-path1.txt')
-	parser.add_argument('--pretrainuserembedding', type=str, dest='pre_train_user_embedding', default='data/mooc/u_vectors1.txt')
-	parser.add_argument('--pretrainmovieembedding', type=str, dest='pre_train_movie_embedding', default='data/mooc/v_vectors1.txt')
-	parser.add_argument('--train', type=str, dest='train_file', default='data/mooc/training1.txt')
-	parser.add_argument('--test', type=str, dest='test_file', default='data/mooc/test1.txt')
-	parser.add_argument('--results', type=str, dest='results', default='data/mooc/results.txt')
-
-	parsed_args = parser.parse_args()
-
-	input_dim = parsed_args.input_dim
-	hidden_dim = parsed_args.hidden_dim
-	out_dim = parsed_args.out_dim
-	iteration = parsed_args.iteration
-	learning_rate = parsed_args.learning_rate
-
-	positive_path = parsed_args.positive_path
-	negative_path = parsed_args.negative_path
-	pre_train_user_embedding = parsed_args.pre_train_user_embedding
-	pre_train_movie_embedding = parsed_args.pre_train_movie_embedding
-	train_file = parsed_args.train_file
-	test_file = parsed_args.test_file
-	results_file = parsed_args.results
-
-	start_time = datetime.now()
-
-	fr_postive = open(positive_path, 'r')
-	fr_negative = open(negative_path, 'r')
-	fr_pre_user = open(pre_train_user_embedding, 'r')
-	fr_pre_movie = open(pre_train_movie_embedding, 'r')
-	fr_train = open(train_file,'r')
-	fr_test = open(test_file,'r')
-	fw_results = open(results_file, 'w')
-
-
-	node_count = 0 #count the number of all entities (user, movie and attributes)
-	all_variables = {} #save variable and corresponding id
-	paths_between_pairs = {} #save all the paths (both positive and negative) between a user-movie pair
-	positive_label = [] #save the positive user-movie pairs
-	all_user = [] #save all the users
-	all_movie = [] #save all the movies
-
-	start_time = datetime.now()
-	load_paths(fr_postive, True)
-	load_paths(fr_negative, False)
-	print ('The number of all variables is :' + str(len(all_variables)))
-	end_time = datetime.now()
-	duration = end_time - start_time
-	print ('the duration for loading user path is ' + str(duration) + '\n')
-
-	start_time = datetime.now()
-	node_size = len(all_variables)
-	print(node_size)
-	pre_embedding = np.random.rand(node_size, input_dim) #embeddings for all nodes
-	print(pre_embedding.shape)
-	load_pre_embedding(fr_pre_user, True)
-	load_pre_embedding(fr_pre_movie, False)
-	pre_embedding = torch.FloatTensor(pre_embedding)
-	end_time = datetime.now()
-	duration = end_time - start_time
-	print ('the duration for loading embedding is ' + str(duration) + '\n')
-	
-
-	start_time = datetime.now()
-	model = LSTMTagger(node_size, input_dim, hidden_dim, out_dim, pre_embedding)
-	if torch.cuda.is_available():
-		model = model.cuda()
-
-	model_train = LSTMTrain(model, iteration, learning_rate, paths_between_pairs, positive_label, \
-		all_variables, all_user, all_movie)
-	embedding_dict = model_train.train()
-	print('model training finished')
-	end_time = datetime.now()
-	duration = end_time - start_time
-	print ('the duration for model training is ' + str(duration) + '\n')
-
-	start_time = datetime.now()
-	train_dict = load_data(fr_train)
-	test_dict = load_data(fr_test)
-	model_evaluation = LSTMEvaluation(embedding_dict, all_movie, train_dict, test_dict)
-	top_score_dict = model_evaluation.calculate_ranking_score()
-	precision_1,_ = model_evaluation.calculate_results(top_score_dict, 1)
-	precision_5,_ = model_evaluation.calculate_results(top_score_dict, 5)
-	precision_10, mrr_10 = model_evaluation.calculate_results(top_score_dict, 10)
-	end_time = datetime.now()
-	duration = end_time - start_time
-	print ('the duration for model evaluation is ' + str(duration) + '\n')
-
-	write_results(fw_results, precision_1, precision_5, precision_10, mrr_10)
-
-	end_time = datetime.now()
-	duration = end_time - start_time
-	print ('the duration for loading item embedding is ' + str(duration) + '\n')
-
-	fr_postive.close()
-	fr_negative.close()
-	fr_pre_user.close()
-	fr_pre_movie.close()
-	fr_train.close()
-	fr_test.close()
-	fw_results.close()
+		return self.dump_post_embedding()
